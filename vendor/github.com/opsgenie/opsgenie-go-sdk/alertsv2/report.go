8b9dc6ef43f31bb8a1be34c73c5ee8fba0674929
Brandon Philips <brandon.philips@coreos.com>
2019-03-21 21:31:56 +0000

    opsgenie-go-sdk: hack int to larger value
    The API is providing a number larger than 32 bits. Whatever.

--- vendor/github.com/opsgenie/opsgenie-go-sdk/alertsv2/report.go
+++ vendor/github.com/opsgenie/opsgenie-go-sdk/alertsv2/report.go
@@ -1,8 +1,8 @@
 package alertsv2
 
 type Report struct {
-	AckTime        int32 `json:"ackTime,omitempty"`
-	CloseTime      int32 `json:"closeTime,omitempty"`
+	AckTime        int    `json:"ackTime,omitempty"`
+	CloseTime      int    `json:"closeTime,omitempty"`
 	AcknowledgedBy string `json:"acknowledgedBy,omitempty"`
 	ClosedBy       string `json:"closedBy,omitempty"`
 }
